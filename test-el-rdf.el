2e3fcef2e5b2388f9e222dd695a0c655cf498954
Ian FitzPatrick <ian@ianfitzpatrick.eu>
2025-08-24 16:02:38 +0200

    updated tests

--- test-el-rdf.el
+++ test-el-rdf.el
@@ -10,11 +10,22 @@
 	       (bob friend charlie)
 	       (alice friend charlie)) test-graph)
 
+
+(ert-deftest test-ask-query()
+  (should
+   (ask '((alice friend bob)) test-graph)
+   ))
+
 (ert-deftest test-select-query ()
   "Test the select function with mutual friendship query"
   (let ((result (select '($x $y) (graph-query '(($x friend $y)($y friend $x)) test-graph))))
     (should
-     (member '((alice . bob)) result))))
+     (-any
+      (lambda (x)
+	(and
+	 (member 'alice x)
+	 (member 'bob x)))
+      result))))
 
 (provide 'test-el-rdf)
 ;;; test-el-rdf.el ends here
