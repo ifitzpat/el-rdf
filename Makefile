a50411405c3645327abceb6400ae7d3d2b708f4f
Ian FitzPatrick <ian@ianfitzpatrick.eu>
2025-08-24 15:05:07 +0200

    update test and add makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	emacs -batch -l ert -l test-el-rdf.el -f ert-run-tests-batch-and-exit
--- test-el-rdf.el
+++ test-el-rdf.el
@@ -6,13 +6,15 @@
 (setq test-graph (make-graph))
 
 (add-triples '((alice friend bob)
+	       (bob friend alice)
 	       (bob friend charlie)
 	       (alice friend charlie)) test-graph)
 
 (ert-deftest test-select-query ()
-  "Test the select function"
-  (should
-   (eq (select '($x $y) (graph-query '(($x friend $y)($y friend $x)))) '(($x . alice)($y . bob)) ) ))
+  "Test the select function with mutual friendship query"
+  (let ((result (select '($x $y) (graph-query '(($x friend $y)($y friend $x)) test-graph))))
+    (should
+     (member '((alice . bob)) result))))
 
 (provide 'test-el-rdf)
 ;;; test-el-rdf.el ends here
