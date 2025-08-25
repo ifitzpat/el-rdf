42f3c964e4b01a6510d389e8bf29e9ceb42745fe
Ian FitzPatrick <ian@ianfitzpatrick.eu>
2025-08-25 23:04:22 +0200

    fix a bug in graph-query

--- el-rdf.el
+++ el-rdf.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ian FitzPatrick ian@ianfitzpatrick.eu
 ;; URL: codeberg.org/ifitzpat/el-rdf
-;; Version: 0.0.9
+;; Version: 0.0.10
 ;; Package-Requires: ((emacs "27.1")(request)(dash "20250312.1307"))
 ;; Keywords: rdf triple-store
 
@@ -127,9 +127,10 @@
         (expand-duals (gethash o (cdr (assoc 'osp graph))) o 'osp) ;; TODO reorder
         )
        (t
-        (ht-map (lambda (key value)
-  		(expand-duals value key))
-  	      (cdr (assoc 'spo graph))))
+        (apply #'append
+               (ht-map (lambda (key value)
+                        (expand-duals value key))
+                      (cdr (assoc 'spo graph)))))
        )))
 
   (defun printgindex (index)
