1ec0090bbce3502797f5ef8a905d5eea579ed06d
Ian FitzPatrick <ian@ianfitzpatrick.eu>
2025-09-02 11:46:27 +0200

    bumped version number

--- el-rdf.el
+++ el-rdf.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ian FitzPatrick ian@ianfitzpatrick.eu
 ;; URL: codeberg.org/ifitzpat/el-rdf
-;; Version: 0.0.14
+;; Version: 0.0.15
 ;; Package-Requires: ((emacs "27.1")(request)(dash "20250312.1307"))
 ;; Keywords: rdf triple-store
 
